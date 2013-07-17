b08163e00d9930dc6bc0928f5cbf00e1f8a3c64f
shivarajkv <shivaraj.kv05@gmail.com>
2013-07-17 09:32:42 +0530

    Overloading example
    Here in line 9 and 10 the indexof method is being overloaded in java api

---
+++ overloading/OverLoadingExample.java
@@ -0,0 +1,14 @@
+package com.shivaraj.training.overloading;
+
+public class OverLoadingExample {
+
+	
+	public static void main(String[] args) {
+
+		String str = "java training course";
+		System.out.println(str.indexOf("j"));
+		System.out.println(str.indexOf("a", 5));
+		
+	}
+
+}
