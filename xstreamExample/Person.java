06fb7d101af4012a35eb937c4616cdb2ec0e63b8
shivarajkv <shivaraj.kv05@gmail.com>
2013-07-28 23:57:26 +0530

    Serialization using xstream

---
+++ xstreamExample/Person.java
@@ -0,0 +1,28 @@
+package com.shivaraj.xstreamExample;
+
+public class Person {
+
+	private String firstName;
+	private String lastName;
+	private PhoneNumber phone;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String fn) {
+		this.firstName = fn;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String ln) {
+		this.lastName = ln;
+	}
+	public PhoneNumber getPhone() {
+		return phone;
+	}
+	public void setPhone(PhoneNumber phone) {
+		this.phone = phone;
+	}
+	
+}
---
+++ xstreamExample/PhoneNumber.java
@@ -0,0 +1,16 @@
+package com.shivaraj.xstreamExample;
+
+public class PhoneNumber {
+
+	private int code;
+	private String number;
+	
+	public PhoneNumber() {
+		
+	}
+	public PhoneNumber(int code,String number){
+		this.code=code;
+		this.number=number;
+	}
+	
+}
