c5a1715a812bfd8c38832fc2715d4ab37b3ee425
shivarajkv <shivaraj.kv05@gmail.com>
2013-07-29 22:06:35 +0530

    StackOver flow error

--- Exception/ExceptionHandler.java
+++ Exception/ExceptionHandler.java
@@ -8,20 +8,7 @@ public class exce {
 	}
 	public static void main(String[] args) {
 		new exce().test();
-		int[] a = null;
-
-		try{
-			System.out.println(a.length);
-		}catch(NullPointerException e){
-			a=new int[5];
-			System.out.println(a.length);
-		}
-
-		try{
-			System.out.println(a[5]);
-		}catch(ArrayIndexOutOfBoundsException e){
-			System.out.println("can access only till "+(a.length-1));
-		}
+		
 	}
 
 }
